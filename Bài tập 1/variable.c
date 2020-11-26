a977b1c9f01c1b4798ee4437a9852130ec791800
Thảo Như <72533740+nhuhy822002@users.noreply.github.com>
2020-11-26 09:44:58 +0700

    Update variable.c

--- Bài tập 1/variable.c
+++ Bài tập 1/variable.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-
 int main(void) {
-	// khai bao bien num kieu int
-	int num;
-	// khoi tao gia tri bien num
-	num = 5;
-	// hien thi bien num ra man hinh
-	printf("Bien num co gia tri: %d\n",num);
-	// khai bao bien x kieu char
-	char c;
-	// khoi tao gia tri cho bien c
-	c = 'a';
-	// hien thi bien c ra man hinh
-	printf("Bien num co gia tri: %d", c);
-	// Hien thi dong; Bien num = 5 va bien c = a
-	printf("Bien num = %d va bien c = %c\n", num, c);
-	getchar();
-	return 0 ;
+  // khai bao bien num kieu int
+  int num;
+  // khoi tao gia tri bien num
+  num = 5;
+  //hien thi bien num ra man hinh
+  printf("Bien num= %d\n",num);
+  // khai bao bien x kieu char
+  char c;
+  // khoi tao gia tri cho bien c
+  c = 'a';
+  // hien thi bien c ra man hinh
+  printf("Bien c = %c\n",c);
+  //hien thi dong: bien num=5 va bien c=a
+  printf("Bien num = %d\n va bien c = %c\n",num,c);
+  printf("Bien num = %d va bien c = %c\n",num,c);
+  return 0;
 }
