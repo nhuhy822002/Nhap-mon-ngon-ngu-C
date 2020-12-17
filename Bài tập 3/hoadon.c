b855e8200acf3ba389f60674dc212ad312ff89ed
Thảo Như <72533740+nhuhy822002@users.noreply.github.com>
2020-12-17 09:15:43 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -11,8 +11,8 @@ int main(){
 	scanf("%f",&dongia);
 	
 	if (soluong>=5){//kiem tra so luong mua >= 5 
-		//giam gia 50% = 500/100
-		thanhtien = (dongia*soluong)*500/100;
+		//giam gia 50% = 50/100
+		thanhtien = (dongia*soluong)*50/100;
 		printf("Thanh tien: %g",thanhtien);
 	}
 	else{//so luong mua tu >=5 
