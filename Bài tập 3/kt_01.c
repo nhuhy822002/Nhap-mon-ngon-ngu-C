d23636c5e2e677abceeed442f8c356c4c9cb5c11
Thảo Như <72533740+nhuhy822002@users.noreply.github.com>
2020-12-31 10:59:40 +0700

    Add files via upload

---
+++ Bài tập 3/kt_01.c
@@ -0,0 +1,26 @@
+// Viet chuong trinh in bang cuu chuong ra man hinh
+
+#include <stdio.h>
+
+int main(void){
+ // khai bao bien
+	int i, j;
+// xuat ra man hinh bang cuu chuong
+	for( i = 1; i<= 10;i = i + 1) {
+		for (j = 1; j <= 10; j++) {
+			printf("%c %d x %2d = %3d",179,j,i,i*j);
+	}
+			printf("%c\n", 179);
+	}
+return 0;
+	
+}
+
+	
+	
+
+	
+
+
+
+
