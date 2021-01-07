bb8f35aeea6b2d28f435fbe1dc72af245c45a97b
Thảo Như <72533740+nhuhy822002@users.noreply.github.com>
2021-01-07 10:50:52 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,17 +1,21 @@
-// Viet chuong trinh in bang cuu chuong ra man hinh
-
-#include <stdio.h>
-
+#include<stdio.h>
 int main(void){
- // khai bao bien
-	int i, j;
-// xuat ra man hinh bang cuu chuong
-	for (i = 1; i<= 10; i = i + 1) {
-	for (j = 1; j<= 10; j++) {
-			printf("%c %d x %2d = %3d",179,j,i,i*j);
-	}
-			printf("%c\n", 179);
-	}
-return 0;
-	
+  int a,b;
+  printf("BANG CUU CHUONG\n");
+  //bang 2
+  printf("bang nhan 2\n");
+  printf("2 x 1 = 2\n");
+  printf("2 x 2 = 4\n");
+  printf("2 x 3 = 6\n");
+  //bang 3
+  printf("bang nhan 3\n");
+  printf("3 x 1 = 3\n");
+  printf("3 x 2 = 3\n");
+  printf("3 x 3 = 3\n");
+  //bang 4
+  printf("bang nhan 4\n");
+  printf("4 x 1 = 4\n");
+  printf("4 x 2 = 8\n");
+  printf("4 x 3 = 12\n");
+
 }
