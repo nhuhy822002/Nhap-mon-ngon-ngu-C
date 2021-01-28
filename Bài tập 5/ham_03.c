16eaa186639258720b725867918848824358252c
Thảo Như <72533740+nhuhy822002@users.noreply.github.com>
2021-01-28 13:51:01 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,15 +1,14 @@
 //Khoi khai bao
 #include<stdio.h>
+int n;int s=0;int sc,sl,ko;//bien toan cuc
 
 void Nhap_pt_mang(int a[]);
 void Hien_thi_pt_mang(int a[]);
 void Hien_thi_tong_chan(int a[]);
 void Dem_chan_le_0(int a[]);
-
+ 
  //khoi ham main
-int n;
 int main(){
-
   printf("Nhap n = ");
   scanf("%d", &n);
   int a[n];
@@ -19,43 +18,48 @@ int main(){
   Dem_chan_le_0(a);
   return 0;
 }
-
-// khoi dinh nghia ham
+//khoi dinh nghia ham
 void Nhap_pt_mang(int a[]){
-	int count;
-	for (count=0;count<n;count++){
-		printf("Nhap a[%d]= ",count);scanf("%d",&a[count]);
-	}
+  for(int i = 0;i < n ; i++){
+    printf("nhap phan tu a [%d]= ",i);
+        scanf("%d", &a[i]);
 }
-
+}
 void Hien_thi_pt_mang(int a[]){
-	int count;
-	printf("mang a:");
-	for (count=0;count<n;count++){
-		printf(" %d ",a[count]);
-	}
-	printf("\n");
+    printf("mang a: ");
+  for(int i=0; i < n; i++){
+      printf(" %d",a[i]);
+      }
 }
-
 void Hien_thi_tong_chan(int a[]){
-	int count,even_sum=0;
-	for (count=0;count<n;count++){
-		if (a[count]%2==0){
-			even_sum+=a[count];
-		}
-	}
-	printf("Tong cac so chan trong mang = %d\n",even_sum);
+for(int i=0;i<n;i++){
+        if(a[i] % 2 == 0){ 
+          s+=a[i];
+        }
+  }
+  printf("\n tong = %d ",s);
 }
-
 void Dem_chan_le_0(int a[]){
-	int count,count_even=0,count_odd=0;
-	for (count=0;count<n;count++){
-		if (a[count]%2==0){
-			count_even+=1;
-		}
-		else{
-			count_odd+=1;
-		}
-	}
-	printf("Mang co %d so chan, %d so le",count_even,count_odd);
+  printf("\nso chan tu mang a: ");
+  for(int i=0;i<n;i++){
+        if(a[i] % 2==0 &&a[i]>0){ 
+        printf("%d ",a[i]);
+        sc+=1;
+        }
+  }
+  printf("\nso le tu mang a: ");
+  for(int i=0;i<n;i++){
+        if(a[i] % 2!=0){
+        printf("%d ",a[i]);
+        sl+=1;
+    }
+    }    
+  for(int i=0;i<n;i++){
+        if(a[i] ==0){
+        ko+=1;
+    }
+  }
+  printf("\nso cac so chan trong mang a: %d ",sc);
+  printf("\nso cac so le trong mang a: %d ",sl);
+  printf("\nso cac so 0: %d ",ko);
 }
